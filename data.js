6d5d4ef080b0aca6a4ba70d32cab51648ce30677
Saurabh Jain <saurabh@skjapp.com>
2012-07-10 21:20:47 +0530

    Some more updates to make the initial structure.

---
+++ data.js
@@ -0,0 +1,7 @@
+var data = 
+{
+	'app1' :
+	{
+		'type' : 'app'
+	}
+}
--- js/engine.js
+++ js/engine.js
@@ -4,7 +4,7 @@
 
 function createEngine()
 {
-	
+	alert(data['app1']['type']) ;
 }
 
 function updateEngine()
@@ -26,16 +26,85 @@ function deleteEngine()
 /////	UI CODE		/////
 /////////////////////////
 
+// PAGE CODE
+function createPage()
+{
+	
+}
+
+function updatePage()
+{
+	
+}
+
+function deletePage()
+{
+	
+}
+
+// LAYER CODE
+function createLayer()
+{
+	
+}
+
+function updateLayer()
+{
+	
+}
+
+function deleteLayer()
+{
+	
+}
+
+// LAYER PART CODE
+function createLayerPart()
+{
+	
+}
+
+function updateLayerPart()
+{
+	
+}
+
+function deleteLayerPart()
+{
+	
+}
 
 /////////////////////////
 /////	CLOUD CODE	/////
 /////////////////////////
+
+// CLOUD CODE
+
+
+// TRANSACTION CODE
+
+
+// MESSAGE CODE
 
 
 /////////////////////////
 /////	USER CODE	/////
 /////////////////////////
 
+// PROFILE CODE
+// CONTACTS CODE
+// LANDMARKS CODE
+// REMINDERS CODE
+// CALENDAR CODE
+// ALARMS CODE
+// MAIL CODE
+// PHOTOS CODE
+// DEVICE SETTINGS CODE
+// VIDEOS CODE
+// MESSAGES CODE
+// NOTES CODE
+// CONTENT CODE
+// TIMELINE CODE
 
 /////////////////////////
 /////	DEVICE CODE	/////
